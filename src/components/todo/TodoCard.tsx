ef72b5fc5d14b3b0cfcd40cbd1b194e6d728b1b2
shagors <shajjadshagor@gmail.com>
2024-02-04 10:28:25 +0600

    api add and data get from api with redux

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,22 +1,39 @@
 import { FilePenLine, Trash2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { useAppDispatch } from "@/redux/hook";
-import { removeTodo } from "@/redux/features/todoSlice";
+import { removeTodo, toggleComplete } from "@/redux/features/todoSlice";
 
 type TTodoCardProps = {
   id: string;
   title: string;
   description: string;
+  isCompleted?: boolean;
 };
 
-const TodoCard = ({ id, title, description }: TTodoCardProps) => {
+const TodoCard = ({ id, title, description, isCompleted }: TTodoCardProps) => {
   const dispatch = useAppDispatch();
+
+  const handleToggleState = () => {
+    dispatch(toggleComplete(id));
+  };
 
   return (
     <div className="bg-white rounded-md flex justify-between items-center p-3 border">
-      <input type="checkbox" />
+      <input
+        onChange={handleToggleState}
+        type="checkbox"
+        name="complete"
+        id="complete"
+      />
       <p className="font-semibold">{title}</p>
       {/* <p>Time</p> */}
+      <div>
+        {isCompleted ? (
+          <p className="text-green-500 font-medium">Done</p>
+        ) : (
+          <p className="text-pink-600 font-medium">Pending</p>
+        )}
+      </div>
       <p>{description}</p>
       <div className="space-x-5">
         <Button onClick={() => dispatch(removeTodo(id))} className="bg-red-500">
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,23 @@ import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  // data get from local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // from server command
+  const { data: todos, error, isLoading } = useGetTodosQuery(undefined);
+
+  if (isLoading) {
+    return (
+      <p className="text-4xl text-gray-500 font-thin text-center mt-10">
+        Loading....
+      </p>
+    );
+  }
+
   return (
     <div className="">
       <div className="flex justify-between mb-6">
@@ -13,8 +27,8 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos?.map((todo) => (
-            <TodoCard key={todo.id} {...todo} />
+          {todos?.data?.map((todo) => (
+            <TodoCard key={todo._id} {...todo} />
           ))}
         </div>
         {/* <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
